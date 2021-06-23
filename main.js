32b580d5f2713e78d5ed6c90cbec0faddef6296d
Donggyun Nam <ndk6879@naver.com>
2021-06-23 16:06:11 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -31,6 +31,14 @@ contactButton.addEventListener('click', (event) => {
     scrollIntoView('#contact');
 })
 
+//Make home opacity gradually transparent
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight
+})
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth' });
