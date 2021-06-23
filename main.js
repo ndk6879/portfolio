8c3dec630c45fd238f818cf6fb2e68f26a7bcba1
Donggyun Nam <ndk6879@naver.com>
2021-06-23 15:35:46 +0900

    JS: Home Contact-Me button

--- main.js
+++ main.js
@@ -21,8 +21,17 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null) {
         return;
     }
-
-    console.log(event.target.dataset.link);    
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    scrollIntoView(link);
 })
+
+
+// Handle Contact-Me button
+const contactButton = document.querySelector('.home__contact');
+contactButton.addEventListener('click', (event) => {
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
