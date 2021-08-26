109462e6a3a43ad2b826534b36aea8a0301a1ab5
Donggyun Nam <ndk6879@naver.com>
2021-08-26 17:16:15 -0400

    HTML: remove skills section. CSS: change section's padding & change category count in button of work. JS: remove skills section in navbar & make navbar work for contact.

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ workBtnContainer.addEventListener('click', (e) => {
 const sectionsIds = [
     '#home', 
     '#about', 
-    '#skills', 
+    // '#skills', 
     '#work', 
     '#contact'
 ];
@@ -142,7 +142,7 @@ sections.forEach(section => observer.observe(section));
 window.addEventListener('wheel', () => {
     if (window.scrollY === 0) {
         selectedNavIndex = 0;
-    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+    } else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight) {
         selectedNavIndex = navItems.length  - 1;
     }
     selectNavItem(navItems[selectedNavIndex]);
